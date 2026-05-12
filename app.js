1a250606f4ec389459bc090de0a0cbf834f68a0a
Manishpande2 <biraalogang@gmail.com>
2026-05-13 05:00:11 +0545

    basic routing

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
 const express = require("express");
+
 const app = express();
 
+app.set("view engine", "ejs");
+
 app.get("/", (req, res) => {
-  res.send("hello message");
+  res.render("home");
+});
+
+app.get("/about", (req, res) => {
+  const name = "manish";
+  const address = "sankhuwasabha";
+  res.render("about", { data: name, address: address });
 });
 
 app.listen(3000, () => {
